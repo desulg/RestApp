c13b88cd98e5a6cc60ad238c4d18dee635d02119
desulg <31885461+desulg@users.noreply.github.com>
2019-04-23 01:06:39 +0300

    color and time

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,86 +6,89 @@ import { Insomnia } from '@ionic-native/insomnia/ngx';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
-
-  percent:number = 0;
-  radius:number = 100;
-  fullTime: any = '00:01:30'
-
-  timer: any = false;
-  progress: any = 0;
-  minutes: number = 1;
-  seconds: any = 30;
-
-  elapsed:any = {
+  
+  elapsed: any = {
     h: '00',
     m: '00',
     s: '00'
   }
-
+  progress:any = 0;
+  overallProgress:any = 0;
+  percent: number = 0;
+  radius: number = 100;
+  minutes: number = 1;
+  seconds: any = 10;
+  timer: any = false;
   overallTimer: any = false;
+  fullTime: any = '00:01:30';
 
-constructor(private insomnia: Insomnia){ 
+ constructor(private insomnia: Insomnia) { 
 
-}
+  }
 
-  startTime(){
+  countDownTimer: any = false;
+  timeLeft: any = {
+    m: '00',
+    s: '00'
+  };
+  remainingTime = `${this.timeLeft.m}:${this.timeLeft.s}`;
 
+ 
+
+  startTime() {
+    
     if(this.timer) {
       clearInterval(this.timer);
-    }
-
+      clearInterval(this.countDownTimer);
+    } 
     if(!this.overallTimer) {
       this.progressTimer();
-      this.insomnia.keepAwake();
+      this.insomnia.keepAwake()
     }
 
-    this.timer = false;
-    this.percent = 0;
-    this.progress = 0;
+      this.timer = false;
+      this.percent = 0;
+      this.progress = 0;
 
-    let timeSplit = this.fullTime.split(':');
-    this.minutes = timeSplit[1];
-    this.seconds = timeSplit[2];
+      let timeSplit = this.fullTime.split(':');
+      this.minutes = timeSplit[1];
+      this.seconds = timeSplit[2];
 
-    let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
-    
-    this.timer = setInterval(() => {
-      
-      if(this.percent == this.radius)
-        clearInterval(this.timer);
+      let totalSeconds = Math.floor(this.minutes * 60) + parseInt(this.seconds);
+      let secondsLeft = totalSeconds;
 
-      this.percent = Math.floor((this.progress / totalSeconds) * 100);
-      this.progress++;
-    }, 1000)
-  }
+      let forwardsTimer = () => {
+        if (this.percent == this.radius) clearInterval(this.timer)
+        this.percent = Math.floor((this.progress / totalSeconds) * 100)
+        ++this.progress
+      }
 
-  progressTimer() {
-    let countDownDate = new Date();
+      let backwardsTimer = () => {
+        if (secondsLeft >= 0) {
+          this.timeLeft.m = Math.floor(secondsLeft / 60)
+          this.timeLeft.s = secondsLeft - (60 * this.timeLeft.m)
+          this.remainingTime = `${this.pad(this.timeLeft.m, 2)}:${this.pad(this.timeLeft.s, 2)}`
+          secondsLeft--;
+        }
+      }
 
-    this.overallTimer = setInterval(() => {
-      let now = new Date().getTime();
-      let distance = now - countDownDate.getTime();
+      // run once when clicked
+      forwardsTimer()
+      backwardsTimer()
 
-      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
-
-      this.elapsed.h = this.pad(this.elapsed.h, 2);
-      this.elapsed.m = this.pad(this.elapsed.m, 2);
-      this.elapsed.s = this.pad(this.elapsed.s, 2);
-
-    }, 1000)
-  }
-  pad(num, size) {
-    let s = num+"";
-    while (s.length < size) s = "0" + s;
-    return s;
+      // timers start 1 second later
+      this.countDownTimer = setInterval(backwardsTimer, 1000)
+      this.timer = setInterval(forwardsTimer, 1000)
   }
 
   stopTime() {
+    clearInterval(this.countDownTimer);
     clearInterval(this.timer);
     clearInterval(this.overallTimer);
+    this.countDownTimer = false;
     this.overallTimer = false;
     this.timer = false;    
     this.percent = 0;
@@ -95,6 +98,46 @@ constructor(private insomnia: Insomnia){
       m: '00',
       s: '00'
     }
+    this.timeLeft = {
+      m: '00',
+      s: '00'
+    }
+    this.remainingTime = `${this.pad(this.timeLeft.m, 2)}:${this.pad(this.timeLeft.s, 2)}`;
     this.insomnia.allowSleepAgain()
   }
+
+  progressTimer() {
+    let countDownDate = new Date();
+
+    this.overallTimer = setInterval(() => {
+      let now = new Date().getTime();
+
+      // Find the distance between now an the count down date
+      var distance = now - countDownDate.getTime();
+
+      // Time calculations for hours, minutes and seconds
+     
+      this.elapsed.h = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.elapsed.m = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.elapsed.s = Math.floor((distance % (1000 * 60)) / 1000);
+      
+      this.elapsed.h = this.pad(this.elapsed.h, 2);
+      this.elapsed.m = this.pad(this.elapsed.m, 2);
+      this.elapsed.s = this.pad(this.elapsed.s, 2);
+
+
+
+    },1000)
+  }
+
+    pad(num, size) {
+      let s = num+"";
+      while (s.length < size) s = "0" + s;
+      return s;
+    }
+
+    updateMyDate($event) {
+      console.log($event.split(":"));
+    }
+  
 }
